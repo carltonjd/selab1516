37ab9799d41fef9d7e731c6394a7c0d5753ca4af
carltonjd <carltonjdsouza@gmail.com>
2015-12-21 16:49:01 +0530

    Testing out the commit

--- ATMClient/src/atm_package/RecieptPrinter.java
+++ ATMClient/src/atm_package/RecieptPrinter.java
@@ -14,6 +14,7 @@ public class RecieptPrinter {
 		System.out.println("Balance : " + balance);
 		System.out.println("Withdrawn Amount : " + withdraw_amt);
 		System.out.println("----------------------------------");
+		System.out.println("----------------------------------");
 
 	}
 
