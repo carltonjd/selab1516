76b4a616102d1a42f0a56d6a1b8308b817c6617d
carltonjd <carltonjdsouza@gmail.com>
2015-12-22 10:47:57 +0530

    Code cleanup

--- Assignment_2_Q/src/QClient.java
+++ Assignment_2_Q/src/QClient.java
@@ -1,35 +1,34 @@
 
 public class QClient {
 	public static void main(String args[]) {
-		Queue<Integer> Q = new Queue<Integer>();
-		Q.enQ(4);
-		System.out.println("Dequeued : " + Q.deQ());
-		System.out.println("Queue empty : " + Q.isEmpty());
-		Q.enQ(5);
-		System.out.println("Queue empty : " + Q.isEmpty());
-		Q.enQ(6);
-		Q.enQ(7);
-		Q.enQ(8);
-		System.out.println("Queue size is  : " + Q.Qsize());
-		while(!Q.isEmpty()){
-			System.out.println("Dequeued : " + Q.deQ());
+		Queue<Integer> integerQueue = new Queue<Integer>();
+		integerQueue.enQ(4);
+		System.out.println("Dequeued : " + integerQueue.deQ());
+		System.out.println("Queue empty : " + integerQueue.isEmpty());
+		integerQueue.enQ(5);
+		System.out.println("Queue empty : " + integerQueue.isEmpty());
+		integerQueue.enQ(6);
+		integerQueue.enQ(7);
+		integerQueue.enQ(8);
+		System.out.println("Queue size is  : " + integerQueue.Qsize());
+		while (!integerQueue.isEmpty()) {
+			System.out.println("Dequeued : " + integerQueue.deQ());
 		}
-		
-//Implement a Q of String Objects
-		
-		Queue<String> QS = new Queue<String>();
-		System.out.println("\nString Q ");
 
-		QS.enQ("A");
-		System.out.println("Dequeued : " + QS.deQ());
-		System.out.println("Queue empty : " + QS.isEmpty());
-		QS.enQ("A");
-		QS.enQ("B");
-		QS.enQ("C");
-		QS.enQ("D");
-		System.out.println("Queue size is  : " + QS.Qsize());
-		while(!QS.isEmpty()){
-			System.out.println("Dequeued : " + QS.deQ());
+		// Implement a Q of String Objects
+
+		Queue<String> stringQueue = new Queue<String>();
+		System.out.println("\nString Queue ");
+		stringQueue.enQ("A");
+		System.out.println("Dequeued : " + stringQueue.deQ());
+		System.out.println("Queue empty : " + stringQueue.isEmpty());
+		stringQueue.enQ("A");
+		stringQueue.enQ("B");
+		stringQueue.enQ("C");
+		stringQueue.enQ("D");
+		System.out.println("Queue size is  : " + stringQueue.Qsize());
+		while (!stringQueue.isEmpty()) {
+			System.out.println("Dequeued : " + stringQueue.deQ());
 		}
 
 	}
